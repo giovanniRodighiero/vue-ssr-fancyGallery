3205cc13c567d54de37f0c80083270a17f1de1ae
Giovanni Rodighiero <giovanni.rodighiero@outlook.it>
2016-11-12 18:24:17 +0100

    moved index

--- src/app.vue
+++ src/app.vue
@@ -1,35 +1,22 @@
 
 <template>
   <div class="my-app">
-    <h1>{{name}}</h1>
-    <h2>clicked: {{clicked}}</h2>
-    <button @click="plus()"> +1 </button>
     <my-cmp :name="name"></my-cmp>
   </div>
 </template>
 
 <script>
 import MyCmp from './myCmp.vue';
-function randNum() {
-  return Math.round(Math.random() * 100);
-}
+
 export default {
   components: {
-    MyCmp,
+    MyCmp
   },
   data() {
     return {
       name: `Giovanni`,
       clicked: 0,
     };
-  },
-  created(){
-    this.clicked++;
-  },
-  methods: {
-    plus: function () {
-      this.clicked++;
-    }
   }
 };
 </script>
